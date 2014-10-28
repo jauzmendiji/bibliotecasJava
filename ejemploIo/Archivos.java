a5164df8d4182abc598837a1332ffae41b5472c3
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-10-28 09:15:27 +0100

    berriro

---
+++ ejemploIo/Archivos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Archivos {
+    public static void main(String arglist[]) throws IOException{
+    String nombre;
+
+	Scanner sc = new Scanner(System.in);
+	System.out.print("\nIntroduce la ruta: ");
+	ruta = sc.next();
+	System.out.print("\nIntroduce un nombre para el archivo: ");
+	nombre = sc.next();
+
+    File archivo = new File (ruta, nombre);
+    archivo.createNewFile();
+	}
+}
